43f996918e163a4148c99e481ee8b2917df292a0
Md. Alim Ul Karim <devorg.bd@gmail.com>
2017-08-21 22:21:38 +0600

    api parameters are defined properly

---
+++ Dashboard/app/bears.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bears: function bears() {
+        console.log(arguments[5]);
+        return {
+            res: "we have got it"
+        }
+    }
+};
+
--- Dashboard/server.js
+++ Dashboard/server.js
@@ -8,6 +8,30 @@ var app = express();
 var morgan = require('morgan');
 var config = require('./package.json');
 var ports = config.port;
+var apiPort = ports.api;
+var url = config.env.urls[config.env.current] + ":" + apiPort;
+var request = require('request');
+
+console.log(url);
+
+var getOptions = {
+    url: url + "/api",
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8',
+    }
+};
+
+var postOptions = {
+    url: url + "/api",
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8',
+    }
+};
+
 
 
 // configure app
@@ -16,7 +40,7 @@ app.use(morgan('dev')); // log requests to the console
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || ports.api; // set our port
+var port = process.env.PORT || apiPort; // set our port
 
 
 
@@ -37,7 +61,7 @@ router.use(function (req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function (req, res) {
-    res.send('im the home page!');
+    res.send('Hello from API!');
 });
 
 
@@ -47,15 +71,47 @@ router.route('/:name')
 
     // create a bear (accessed at POST http://localhost:8080/bears)
     .post(function (req, res) {
-        res.send("bears");
-        console.log(req);
+        callfunction(req, res, postOptions);
+
     })
 
     // get all the bears (accessed at GET http://localhost:8080/api/bears)
     .get(function (req, res) {
-        res.send("bears");
-        console.log(req);
+        callfunction(req, res, getOptions);
+        //res.send("bears" + JSON.stringify(getResponseFromFile(req.params.name)));
     });
+
+router.route('/:name/:p1/:p2/:p3')
+
+    // create a bear (accessed at POST http://localhost:8080/bears)
+    .post(function (req, res) {
+        callfunction(req, res, postOptions);
+
+    })
+
+    // get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function (req, res) {
+        callfunction(req, res, getOptions);
+        //res.send("bears" + JSON.stringify(getResponseFromFile(req.params.name)));
+    });
+
+
+var getResponseFromFile = function (name) {
+    if (name) {
+        return require("./app/" + name + ".js");
+    }
+}
+
+var callfunction = function (req, res, options) {
+    var name = req.params.name;
+    console.log(name);
+    if (name) {
+        var fileJs = getResponseFromFile(name);
+        request(options, function () {
+            res.send(JSON.stringify(fileJs[name].apply(this, [app, router, config, options, req, req.params])));
+        });
+    }
+}
 
 router.route('/example')
     // get all the bears (accessed at GET http://localhost:8080/api/bears)
