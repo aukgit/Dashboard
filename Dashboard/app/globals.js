c506ac6e3038a7612962da598a303efc780736cd
Md. Alim Ul Karim <devorg.bd@gmail.com>
2017-08-22 11:39:30 +0600

    chnages from changing to client to http

--- Dashboard/app/globals.js
+++ Dashboard/app/globals.js
@@ -1,6 +1,31 @@
 module.exports = {
     isEmpty: function (o) {
         return o === undefined || o === null;
+    },
+
+    defferedClientPost: function(url, headerArgs, onCompleteFunction, acceptingStatus, checkDurationInMilisec) {
+        var Client = require('node-rest-client').Client;
+        var sleep = require('system-sleep');
+        var client = new Client();
+        var result = null;
+
+        if (acceptingStatus) {
+            acceptingStatus = 200;
+        }
+
+        if (checkDurationInMilisec) {
+            checkDurationInMilisec = 450;
+        }
+
+        client.post(url,
+            headerArgs,
+            function (data, response) {
+                if (response.statusCode === acceptingStatus) {
+                    
+                } else {
+                    throw "Login failed :(";
+                }
+            });
     }
 };
 
